fd42d6a6cd7bc175cb83c588028cb79deeb129b5
hammad-ah <muhammadhammadraza5@gmail.com>
2024-12-29 18:30:59 +0500

    try catch

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,72 @@ const {getAllUsers} = require('../controller/user.controller')
 
 const route = express.Router();
 
+let user01 ={
+    name:"hammad",
+    id:1,
+    email:"hammad@example.com",
+    contact:"03035912244"
+    }
+    let user02 ={
+        name:"ubaid",
+        id:2,
+        email:"ubaid@example.com",
+        contact:"03121047036"
+        }
+        let user03 ={
+            name:"faiz",
+            id:3,
+            email:"faiz@example.com",
+            contact:"03173734451"
+            }
+    let user04 ={
+        name:"hadi",
+        id:4,
+        email:"hadi@example.com",
+        contact:"03035912224"
+        }
+        let user05 ={
+            name:"ibrahim",
+            id:5,
+            email:"ibrahim@example.com",
+            contact:"03121117682"
+            }
+    
+    let users = [user01,user02,user03,user04,user05];
+
+     const getAllUsersById = (req , res) =>{
+       try{ let id = req.params.id;
+        let findUsers = users.filter((user) => {
+            if(user.id == id){
+                return user
+
+
+            }
+
+
+        })
+        let userName = findUsers[0].name;
+            res.status(200).json({message: "success ", data:users})}
+       catch(e){
+        console.log("expection", e);
+
+       }
+        }
+
 route.get('/', getAllUsers);
+
+route.get('/users/:id', (req , res) => {
+    try{
+        res.status(200).json({message: 'success 2'})
+    
+    
+    }
+    catch(e){
+        console.log("expection", e);
+    }
+})
+
+module.exports  = {getAllUsers, getAllUsersById};
 
 
 module.exports = route;
